8f5adccab8c925a186c0cf3b1a6ad5fd8c33ab22
ATpoint <a.toenges@uni-muenster.de>
2022-01-22 23:57:41 +0100

    now output one QC for RNA and one for FBs

--- bin/alevin_qc.R
+++ bin/alevin_qc.R
@@ -7,10 +7,15 @@ suppressMessages({
 })
 
 args=commandArgs(trailingOnly=TRUE)
-if (length(args)!= 2) {
+if (length(args)!= 3) {
   stop("[invalid params]", "\n",
-       "Usage: alevin_qc.R <basename> <paths_to_alevins>")
+       "Usage: alevin_qc.R <basename> <paths_to_alevins> <suffix>")
 }
 
-alevinQCReport(baseDir=args[2], sampleId=args[1], outputFile=paste0(args[1], ".html"),
+if(args[3]=="empty") {
+  suffix <- ""
+} else suffix <- args[3]
+sampleid <- paste0(args[1], suffix)
+
+alevinQCReport(baseDir=args[2], sampleId=sampleid, outputFile=paste0(sampleid, ".html"),
                outputDir=getwd(), outputFormat="html_document", quiet=TRUE)
